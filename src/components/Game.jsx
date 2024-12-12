e3ef095c2db5acb08700a815645cc497c51ec413
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-12-11 21:30:38 -0800

    Finish memory card game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,33 +1,82 @@
 import { useEffect, useState } from "react";
 import "../styles/Game.css";
+import Card from "./Card";
 
 function Header({ scoreboard }) {
+  // will put best score in localStorage eventually
+  const { score, best } = scoreboard;
+
   return (
     <header>
       <h1>Memory Card 🐱</h1>
 
       <div className="scoreboard">
-        <span className="score">Score: {scoreboard.score}</span>
-        <span className="best">Best: {scoreboard.best}</span>
+        <span className="score">Score: {score}</span>
+        <span className="best">Best: {best}</span>
       </div>
     </header>
   );
 }
 
+function shufflePics(pics) {
+  const picsCopy = [...pics];
+
+  for (let i = picsCopy.length - 1; i >= 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    const temp = picsCopy[i];
+
+    picsCopy[i] = picsCopy[randomIndex];
+    picsCopy[randomIndex] = temp;
+  }
+
+  return picsCopy;
+}
+
+function resetPicClicks(pics) {
+  const picsCopy = [...pics];
+
+  for (let i = 0; i < picsCopy.length; i++) {
+    const currentPic = picsCopy[i];
+
+    currentPic.clicked = false;
+  }
+
+  return picsCopy;
+}
+
 function Game() {
   const [catPics, setCatPics] = useState([]);
-  const [scoreboard, setScoreboard] = useState({ score: 0, best: 0 });
-
-  const numOfCards = 3; // strict mode off : 6
-  const catCards = catPics.map((url, index) => {
-    return (
-      <button
-        type="button"
-        key={index}
-        style={{ backgroundImage: `url(${url})` }}
-      ></button>
-    );
+  const [scoreboard, setScoreboard] = useState({
+    score: 0,
+    best: localStorage.getItem("best-score") || 0,
   });
+
+  const { score, best } = scoreboard;
+  const numOfCards = 3; // strict mode off : 6
+
+  function checkCard(cardIndex) {
+    const selectedCard = catPics[cardIndex];
+
+    if (selectedCard.clicked === false) {
+      const catPicsCopy = [...catPics];
+      const updatedCatPic = { ...selectedCard, clicked: true };
+
+      catPicsCopy[cardIndex] = updatedCatPic;
+
+      const shuffledPics = shufflePics(catPicsCopy);
+
+      setScoreboard({ ...scoreboard, score: score + 1 });
+      setCatPics(shuffledPics);
+    } else {
+      const catPicsCopy = [...catPics];
+
+      const reset = resetPicClicks(catPicsCopy);
+      const shuffledReset = shufflePics(reset);
+
+      setScoreboard({ ...scoreboard, score: 0 });
+      setCatPics(shuffledReset);
+    }
+  }
 
   useEffect(() => {
     (async function getCatPics() {
@@ -40,13 +89,32 @@ function Game() {
           const imgFile = await response.blob();
           const imgUrl = URL.createObjectURL(imgFile);
 
-          setCatPics((catPics) => [...catPics, imgUrl]);
+          setCatPics((catPics) => [
+            ...catPics,
+            { clicked: false, url: imgUrl },
+          ]);
         }
       }
     })();
   }, []);
 
-  if (catPics.length !== 6) {
+  const catCards = catPics.map((pic, index) => {
+    return (
+      <Card
+        key={index}
+        imgUrl={pic.url}
+        clickHandler={() => checkCard(index)}
+      />
+    );
+  });
+
+  if (score > best) {
+    setScoreboard({ ...scoreboard, best: score });
+
+    localStorage.setItem("best-score", score);
+  }
+
+  if (score === 0 && catPics.length !== 6) {
     return (
       <>
         <p className="loading">Loading...</p>
