9cd2b98b918214ad8f8607d522dfbdd27f3a7a68
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-12-11 22:00:16 -0800

    Remove strict mode for vercel
    Resolving this merge conflict apparently broke everything. Perhaps my site will work on vercel after this.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Game from "./components/Game.jsx";
-import { StrictMode } from "react";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Game />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<Game />);
